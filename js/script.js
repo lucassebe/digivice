b78d4df2cd8f1ed90df7fb488a870046bab9f51b
Lucas Sebe <lucassebe16@gmail.com>
2023-05-12 14:22:32 -0300

    add node packages, add description nd level

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const img = document.querySelector('#digimon-img');
 const title = document.querySelector('.card-title');
 const text = document.querySelector('.card-text');
 const form = document.querySelector('.form');
+const level =  document.querySelector('#level');
+const description = document.querySelector('#description');
 
 const fetchDigimon = async (digimon) => {
     const APIResponse = await fetch(`https://digimon-api.com/api/v1/digimon/${digimon}`);
@@ -23,6 +25,7 @@ const fetchDigimon = async (digimon) => {
             }).showToast();
     }
 }
+
 const render = async (digimon) => {
 
     const data = await fetchDigimon(digimon);
@@ -30,11 +33,12 @@ const render = async (digimon) => {
     text.innerText = `${data.name} ID n° ${data.id}`;
     title.innerHTML = data.name;
     img.src = data['images'][0]['href'];
+    level.innerHTML = `<strong><span class="descricao">Tipo:</span></strong> ${data['levels'][0].level}`;
+    description.innerHTML = `<strong><span class="descricao">Descrição:</span></strong> ${data.descriptions[0].description}`;
 }
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     
     render(input.value);
-    
     input.value = ''
 });
